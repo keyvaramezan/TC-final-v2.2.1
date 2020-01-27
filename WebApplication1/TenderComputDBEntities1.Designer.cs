a92bc45830af2855646f6f6b8571184f4feaf16d
k1 <keyvanramezan@gmail.com>
2020-01-27 22:52:48 +0330

    create data base from model and add update pannel

--- WebApplication1/TenderComputDBEntities1.Designer.cs
+++ WebApplication1/TenderComputDBEntities1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\TC final v2.2.1\TC final v2.2\WebApplication1\TenderComputDBEntities1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\k1\Source\Repos\keyvaramezan\TC-final-v2.2.1\WebApplication1\TenderComputDBEntities1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
